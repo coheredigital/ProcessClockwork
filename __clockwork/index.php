f04978abf2398dec45fed0825a27f396c56aa3ea
Adam Spruijt <adamspruijt@gmail.com>
2015-12-02 12:09:06 -0500

    added cleanup to index and some comments for clarity

--- __clockwork/index.php
+++ __clockwork/index.php
@@ -1,10 +1,20 @@
 <?php 
 header('Content-Type: application/json');
+
+// include PW api
 include '../index.php';
-$id = $_GET['id'];
+
+// get and sanitize id
+$id = $this->input->get->text("id");
 
 // prevent storing this request
 $wire->clockwork->storeRequest = false;
+
+// run cleanup, running here shpuld mean cleanup only runs when a dev is using clockwork
+// simple way to minimize overhead with setting up crons, etc
+$wire->clockwork->cleanup();
+
+// retireve data if found
 if ($data = $wire->clockwork->storage->retrieve($id)) {
 	echo $data->toJson();
 }
